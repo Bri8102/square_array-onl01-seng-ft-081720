420b35f49927f6bf9f13998e41f7677713d3e9b4
Briana Oniru <damioniru16@gmail.com>
2020-08-20 00:25:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 def square_array(array)
-  # your code here
-  array.each do | number|
-    squared_arr = Array.new
-     Array.new(array([ number * 2]))
-
+  array.each do |number|
+  squared_arr = Array.new
+   array([ number * 2]) 
   end
 end
