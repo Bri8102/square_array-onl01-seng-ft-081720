87d51b36bc8b86c641f51b80cfcaa6e92f5774c6
Briana Oniru <damioniru16@gmail.com>
2020-08-20 00:15:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,10 @@
 def square_array(array)
   # your code here
-
+  square_array.each do | array |
+    array * 2
+  end
 end
+
+
+  #square_array.each do | array |
+  #return square_array([array ** 2]) 
